7906619511571ada7a6af9d459bf71389f8fe937
Christabel Sebastian <christabel.escarez@gmail.com>
2021-01-05 10:37:32 -0800

    files are currently at state from roundtable

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import Form from "./components/Form";
+import Results from "./components/Results";
+import "./styles.css";
+
+// SET UP STATE FOR A NEW USER
+// SET UP STATE FOR A LIST OF USERS
+// CREATE A FUNCTION TO STORE INPUT FIELD DATA
+  //onChange in Form.js --> updateUser
+  //onClick in App.js --> onUserCallback
+  //
+// WHEN NEW USER DATA IS SUBMITTED, ADD IT TO OUR LIST OF USERS
+// DISPLAY LIST OF USERS IN RESULTS
+
+// ADD BONUSES
+
+export default function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const addUser = (newUser) => {
+    const newUsers = [...users, newUser];
+    //create new user?
+    setUsers(newUsers);
+  }
+
+  return (
+    <div className="App">
+      <Form onUserCallback={addUser} />
+      <Results />
+    </div>
+  );
+}
